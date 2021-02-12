ea85e036a0505ab9b936aa864495c6e52c67040b
Lavanya <lavanyace929@gmail.com>
2021-02-12 14:37:18 +0530

    Refactor:Employee daily Wage is CalCulated Using Switch Case

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -8,14 +8,18 @@ public class EmpWageComputation {
 		int empHrs = 0;
 		int empWage = 0;
 		//Computation
-		double empCheck = Math.floor(Math.random() *10) %3;
-		if (empCheck == IS_FULL_TIME)
-			empHrs = 8;
-		else if (empCheck == IS_PART_TIME)
-			empHrs = 4;
-		else
-			empHrs = 0;
-			empWage = empHrs * WAGE_PER_HR;
+		int empCheck = (int)Math.floor(Math.random() *10) %3;
+		switch (empCheck) {
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			default:
+				empHrs = 0;
+		}
+		empWage = empHrs * WAGE_PER_HR;
 		System.out.println("Employee Wage is =" +empWage);
 	}	
 }
