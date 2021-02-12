147db72949439b61d3a714a53775c7095a9be586
Lavanya <lavanyace929@gmail.com>
2021-02-12 14:42:46 +0530

    Added:Employee Monthly Wage Is Calculated

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,25 +2,31 @@ public class EmpWageComputation {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int WAGE_PER_HR = 20;
+	public static final int WORKING_DAYS = 20;
 
 	public static void main(String[] args)
 	{
 		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
 		//Computation
-		int empCheck = (int)Math.floor(Math.random() *10) %3;
-		switch (empCheck) {
-			case IS_FULL_TIME:
-				empHrs = 8;
-				break;
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
-			default:
-				empHrs = 0;
+		for (int day = 0; day < WORKING_DAYS; day++) {
+			int empCheck = (int)Math.floor(Math.random() *10) %3;
+			switch (empCheck) {
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
+			}
+			empWage = empHrs * WAGE_PER_HR;
+			totalEmpWage += empWage;
+			System.out.println("Employee Wage is =" +empWage);
 		}
-		empWage = empHrs * WAGE_PER_HR;
-		System.out.println("Employee Wage is =" +empWage);
+		System.out.println("Total Monthly Wage =" + totalEmpWage);
 	}	
 }
 
