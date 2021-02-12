db3f601e1e0acbf9c26c7aa0b645cc2398a19483
Lavanya <lavanyace929@gmail.com>
2021-02-12 14:20:55 +0530

    Added:Employee Daily Wage Is Calculated

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,14 +1,22 @@
-public class EmpWageComputation {
-	public static final int  IS_PRESENT = 1;
+public class EmpWageComputation
+{
+	public static final int IS_PRESENT = 1;
+	public static final int WAGE_PER_HR = 20;
 
 	public static void main(String[] args)
 	{
+		//Variables
+		int empHrs = 0;
+		int empWage = 0;
 		//Computation
 		double empCheck = Math.floor(Math.random() *10) %2;
 		if (empCheck == IS_PRESENT)
-			System.out.println("Employee is Present");
+			empHrs = 8;
 		else
-			System.out.println("Employee is Abscent");
+			empHrs = 0;
+			empWage = empHrs * WAGE_PER_HR;
+			System.out.println("Employee Wage is =" +empWage);
 	}	
 }
+
 
